c48753feb177cedd2d320a45909a3ece6cb700e7
李進興 Vulcan <vulcan@miniasp.com>
2018-04-17 10:26:22 +0800

    Xamarin.Forms ListView 集合資料多選

---
+++ XFMulSelLV/Solution/XFMulSelLV/XFMulSelLV/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,78 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+
+namespace XFMulSelLV.ViewModels
+{
+    public class Person : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public bool IsSelected { get; set; }
+
+    }
+
+    public class MainPageViewModel : INotifyPropertyChanged, INavigationAware
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public ObservableCollection<Person> PeopleCollection { get; set; } = new ObservableCollection<Person>();
+        public Person PersonSelected { get; set; }
+        public string Title { get; set; } 
+        public DelegateCommand PersonTappedCommand { get; set; }
+        public DelegateCommand ShowResultCommand { get; set; }
+        private readonly INavigationService _navigationService;
+
+        public MainPageViewModel(INavigationService navigationService)
+        {
+            _navigationService = navigationService;
+            PersonTappedCommand = new DelegateCommand(() =>
+            {
+                PersonSelected.IsSelected = !PersonSelected.IsSelected;
+            });
+            ShowResultCommand = new DelegateCommand(() =>
+            {
+                var fooCount = 0;
+                foreach (var item in PeopleCollection)
+                {
+                    if(item.IsSelected == true)
+                    {
+                        fooCount++;
+                    }
+                }
+                Title = $"共選擇 {fooCount} 筆紀錄";
+            });
+        }
+
+        public void OnNavigatedFrom(NavigationParameters parameters)
+        {
+
+        }
+
+        public void OnNavigatingTo(NavigationParameters parameters)
+        {
+
+        }
+
+        public void OnNavigatedTo(NavigationParameters parameters)
+        {
+            PeopleCollection.Clear();
+            for (int i = 0; i < 100; i++)
+            {
+                PeopleCollection.Add(new Person
+                {
+                    Name = $"Name{i}",
+                    Age = i,
+                    IsSelected = false
+                });
+            }
+        }
+
+    }
+}
---
+++ XFMulSelLV/Starter/XFMulSelLV/XFMulSelLV/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,46 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+
+namespace XFMulSelLV.ViewModels
+{
+    public class Person : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+    }
+
+    public class MainPageViewModel : INotifyPropertyChanged, INavigationAware
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        private readonly INavigationService _navigationService;
+
+        public MainPageViewModel(INavigationService navigationService)
+        {
+            _navigationService = navigationService;
+        }
+
+        public void OnNavigatedFrom(NavigationParameters parameters)
+        {
+
+        }
+
+        public void OnNavigatingTo(NavigationParameters parameters)
+        {
+
+        }
+
+        public void OnNavigatedTo(NavigationParameters parameters)
+        {
+        }
+
+    }
+}
