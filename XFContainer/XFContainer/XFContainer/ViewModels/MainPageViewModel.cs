f5d3059b8fac8f19b51aa2550b542ae17ff9d232
李進興 Vulcan <vulcan@miniasp.com>
2018-05-05 20:36:27 +0800

    客製化一個容器內已經裝了多少容量的視覺效果

---
+++ XFContainer/XFContainer/XFContainer/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,99 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Navigation;
+using SkiaSharp;
+using SkiaSharp.Views.Forms;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+namespace XFContainer.ViewModels
+{
+    public class MainPageViewModel : INotifyPropertyChanged, INavigationAware
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        //public delegate void InvalidateSurfaceDelegate();
+        //public InvalidateSurfaceDelegate InvalidateSurfaceVM;
+        public Action InvalidateSurfaceVM;
+        public double BoxViewValue { get; set; }
+        public double BoxViewContainerHeight { get; set; }
+        public DelegateCommand BoxViewValueChangedCommand { get; set; }
+        public DelegateCommand<SKPaintSurfaceEventArgs> PaintSurfaceCommand { get; set; }
+        private readonly INavigationService _navigationService;
+
+        public MainPageViewModel(INavigationService navigationService)
+        {
+            _navigationService = navigationService;
+
+            BoxViewValueChangedCommand = new DelegateCommand(() =>
+            {
+                double containerHeightDP = 294;
+                BoxViewContainerHeight = containerHeightDP * (BoxViewValue / 100.0);
+                InvalidateSurfaceVM?.Invoke();
+            });
+
+            PaintSurfaceCommand = new DelegateCommand<SKPaintSurfaceEventArgs>(args =>
+            {
+                SKImageInfo info = args.Info;
+                SKSurface surface = args.Surface;
+                SKCanvas canvas = surface.Canvas;
+
+                canvas.Clear();
+
+                SKPaint fooOutsideBoxPaint = new SKPaint
+                {
+                    Style = SKPaintStyle.Stroke,
+                    Color = SKColors.Black,
+                    StrokeWidth = 3,
+                };
+                SKPaint fooInsideBoxPaint = new SKPaint
+                {
+                    Style = SKPaintStyle.Fill,
+                    Color = SKColors.White,
+                    IsStroke = false,
+                };
+                SKPaint fooBoxValuePaint = new SKPaint
+                {
+                    Style = SKPaintStyle.Fill,
+                    Color = SKColors.LightPink,
+                    IsStroke = false,
+                };
+
+                float fooScale = info.Width / 100.0f;
+                canvas.Scale(fooScale);
+
+                canvas.DrawRect(0, 0, 100, 300, fooInsideBoxPaint);
+                canvas.DrawRect(3, 3, 94, 294, fooOutsideBoxPaint);
+
+                float containerHeightDP = 294f;
+                float SkiBoxContainerHeight = containerHeightDP * ((float)BoxViewValue / 100.0f);
+                var fooX = 4.0f;
+                var fooY = containerHeightDP - SkiBoxContainerHeight+3;
+                var fooW = 92f;
+                var fooH = SkiBoxContainerHeight;
+                Debug.WriteLine($"X:{fooX} Y:{fooY} W:{fooW} H:{fooH}");
+
+                canvas.DrawRect(fooX, fooY, fooW, fooH, fooBoxValuePaint);
+            });
+        }
+
+        public void OnNavigatedFrom(NavigationParameters parameters)
+        {
+
+        }
+
+        public void OnNavigatingTo(NavigationParameters parameters)
+        {
+
+        }
+
+        public void OnNavigatedTo(NavigationParameters parameters)
+        {
+
+        }
+
+    }
+}
---
+++ XFContainer/XFContainer/XFContainer/Views/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using XFContainer.ViewModels;
+
+namespace XFContainer.Views
+{
+	public partial class MainPage : ContentPage
+	{
+        public MainPage ()
+		{
+			InitializeComponent ();
+            this.BindingContextChanged += (s, e) =>
+            {
+                var foo = s as MainPage;
+                if(foo != null)
+                {
+                    if(foo.BindingContext != null)
+                    {
+                        var VM = this.BindingContext as MainPageViewModel;
+                        VM.InvalidateSurfaceVM = this.skCanvasView.InvalidateSurface;
+                    }
+                }
+            };
+		}
+    }
+}
