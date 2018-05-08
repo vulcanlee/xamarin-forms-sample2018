0df1fde89f5ab590952a09ecc99196b3000a8e63
李進興 Vulcan <vulcan@miniasp.com>
2018-05-08 20:37:17 +0800

    取得裝置ID與相關資訊

---
+++ XFUID/XFUID/XFUID/ViewModels/CrossDeviceInfoVM.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace XFUID.ViewModels
+{
+    public class CrossDeviceInfoVM : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public string AppId { get; set; }
+        public string Id { get; set; }
+        public string Model { get; set; }
+        public string Manufacturer { get; set; }
+        public string DeviceName { get; set; }
+        public string Version { get; set; }
+        public string VersionNumber { get; set; }
+        public string AppVersion { get; set; }
+        public string Platform { get; set; }
+        public string Idiom { get; set; }
+        public string IsDevice { get; set; }
+    }
+}
---
+++ XFUID/XFUID/XFUID/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,59 @@
+﻿using Plugin.DeviceInfo;
+using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Navigation;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+
+namespace XFUID.ViewModels
+{
+    public class MainPageViewModel : INotifyPropertyChanged, INavigationAware
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public CrossDeviceInfoVM DeviceInfo { get; set; }
+        private readonly INavigationService _navigationService;
+        public DelegateCommand GetDeviceInfoCommand { get; set; }
+        public MainPageViewModel(INavigationService navigationService)
+        {
+            _navigationService = navigationService;
+
+            GetDeviceInfoCommand = new DelegateCommand(() =>
+            {
+                GetDeviceInfo();
+            });
+        }
+
+        public void OnNavigatedFrom(NavigationParameters parameters)
+        {
+
+        }
+
+        public void OnNavigatingTo(NavigationParameters parameters)
+        {
+
+        }
+
+        public void OnNavigatedTo(NavigationParameters parameters)
+        {
+        }
+
+        void GetDeviceInfo()
+        {
+            DeviceInfo = new CrossDeviceInfoVM();
+            DeviceInfo.AppId = CrossDeviceInfo.Current.GenerateAppId(true, "Vulcan", "Lee");
+            DeviceInfo.Id = CrossDeviceInfo.Current.Id;
+            DeviceInfo.Model = CrossDeviceInfo.Current.Model;
+            DeviceInfo.Manufacturer = CrossDeviceInfo.Current.Manufacturer;
+            DeviceInfo.DeviceName = CrossDeviceInfo.Current.DeviceName;
+            DeviceInfo.Version = CrossDeviceInfo.Current.Version;
+            DeviceInfo.VersionNumber = CrossDeviceInfo.Current.VersionNumber.ToString();
+            DeviceInfo.AppVersion = CrossDeviceInfo.Current.AppVersion;
+            DeviceInfo.Platform = CrossDeviceInfo.Current.Platform.ToString();
+            DeviceInfo.Idiom = CrossDeviceInfo.Current.Idiom.ToString();
+            DeviceInfo.IsDevice = CrossDeviceInfo.Current.IsDevice.ToString();
+        }
+    }
+}
