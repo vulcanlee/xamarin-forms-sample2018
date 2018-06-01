fd816d884b76be13f1f01f29121cc3d9bb809476
李進興 Vulcan <vulcan@miniasp.com>
2018-06-01 11:41:25 +0800

    Xamarin.Android 執行時期的權限處理說明 2 使用 Plugin.Permissions

---
+++ XFPermPlugin/XFPermPlugin/XFPermPlugin.Android/MainActivity.cs
@@ -0,0 +1,43 @@
+﻿using Android.App;
+using Android.Content.PM;
+using Android.OS;
+using Prism;
+using Prism.Ioc;
+
+[assembly: UsesPermission(Name = Android.Manifest.Permission.ReadExternalStorage)]
+[assembly: UsesPermission(Name = Android.Manifest.Permission.WriteExternalStorage)]
+[assembly: UsesPermission(Name = Android.Manifest.Permission.Camera)]
+[assembly: UsesPermission(Name = Android.Manifest.Permission.CallPhone)]
+namespace XFPermPlugin.Droid
+{
+    [Activity(Label = "XFPermPlugin", Icon = "@mipmap/ic_launcher", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            base.OnCreate(bundle);
+
+            Plugin.CurrentActivity.CrossCurrentActivity.Current.Init(this, bundle);
+
+            global::Xamarin.Forms.Forms.Init(this, bundle);
+            LoadApplication(new App(new AndroidInitializer()));
+        }
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [Android.Runtime.GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Plugin.Permissions.PermissionsImplementation.Current.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+
+    public class AndroidInitializer : IPlatformInitializer
+    {
+        public void RegisterTypes(IContainerRegistry container)
+        {
+            // Register any platform specific implementations
+        }
+    }
+}
+
---
+++ XFPermPlugin/XFPermPlugin/XFPermPlugin/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,52 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Navigation;
+using Prism.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace XFPermPlugin.ViewModels
+{
+    public class MainPageViewModel : ViewModelBase
+    {
+        public DelegateCommand CheckPermissionCommand { get; set; }
+        public readonly IPageDialogService _dialogService;
+        public MainPageViewModel(INavigationService navigationService,
+            IPageDialogService dialogService) 
+            : base (navigationService)
+        {
+            _dialogService = dialogService;
+            Title = "Main Page";
+            CheckPermissionCommand = new DelegateCommand(async () =>
+            {
+                var fooStatus = await Plugin.Permissions.CrossPermissions
+                .Current.CheckPermissionStatusAsync(Plugin.Permissions.Abstractions.Permission.Storage);
+                if (fooStatus != Plugin.Permissions.Abstractions.PermissionStatus.Granted)
+                {
+                    if (await Plugin.Permissions.CrossPermissions.Current.ShouldShowRequestPermissionRationaleAsync
+                    (Plugin.Permissions.Abstractions.Permission.Storage))
+                    {
+                        await _dialogService.DisplayAlertAsync("Need Storage", "Gunna need that Storage", "OK");
+                    }
+
+                    var results = await Plugin.Permissions.CrossPermissions.Current.RequestPermissionsAsync
+                    (Plugin.Permissions.Abstractions.Permission.Storage);
+                    //Best practice to always check that the key exists
+                    if (results.ContainsKey(Plugin.Permissions.Abstractions.Permission.Storage))
+                        fooStatus = results[Plugin.Permissions.Abstractions.Permission.Storage];
+                }
+
+                if (fooStatus == Plugin.Permissions.Abstractions.PermissionStatus.Granted)
+                {
+                    await _dialogService.DisplayAlertAsync("Storage Allow", "You can continue to call any storage API.", "OK");
+                }
+                else if (fooStatus != Plugin.Permissions.Abstractions.PermissionStatus.Unknown)
+                {
+                    await _dialogService.DisplayAlertAsync("Storage Denied", "Can not continue, try again.", "OK");
+                }
+            });
+        }
+    }
+}
