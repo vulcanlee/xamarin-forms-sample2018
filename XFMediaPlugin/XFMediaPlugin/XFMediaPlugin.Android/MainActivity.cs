5b80c027bc27aa57d9adf9f010f424926e37e9e2
李進興 Vulcan <vulcan@miniasp.com>
2018-06-15 18:33:33 +0800

    Xamarin.Forms 使用 MediaPlugin 進行拍照並且顯示在螢幕上

---
+++ XFMediaPlugin/XFMediaPlugin/XFMediaPlugin.Android/MainActivity.cs
@@ -0,0 +1,42 @@
+﻿using Android.App;
+using Android.Content.PM;
+using Android.OS;
+using Prism;
+using Prism.Ioc;
+
+[assembly: UsesPermission(Name = Android.Manifest.Permission.ReadExternalStorage)]
+[assembly: UsesPermission(Name = Android.Manifest.Permission.WriteExternalStorage)]
+[assembly: UsesPermission(Name = Android.Manifest.Permission.Camera)]
+
+namespace XFMediaPlugin.Droid
+{
+    [Activity(Label = "XFMediaPlugin", Icon = "@mipmap/ic_launcher", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            base.OnCreate(bundle);
+
+            Plugin.CurrentActivity.CrossCurrentActivity.Current.Init(this, bundle);
+
+            global::Xamarin.Forms.Forms.Init(this, bundle);
+            LoadApplication(new App(new AndroidInitializer()));
+        }
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, Android.Content.PM.Permission[] grantResults)
+        {
+            Plugin.Permissions.PermissionsImplementation.Current.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+
+    public class AndroidInitializer : IPlatformInitializer
+    {
+        public void RegisterTypes(IContainerRegistry container)
+        {
+            // Register any platform specific implementations
+        }
+    }
+}
+
---
+++ XFMediaPlugin/XFMediaPlugin/XFMediaPlugin/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,79 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace XFMediaPlugin.ViewModels
+{
+    using System.ComponentModel;
+    using Prism.Events;
+    using Prism.Navigation;
+    using Prism.Services;
+    using Xamarin.Forms;
+
+    public class MainPageViewModel : INotifyPropertyChanged, INavigationAware
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public ImageSource MyImageSource { get; set; } = ImageSource.FromFile("DefaultImage.jpg");
+        public DelegateCommand TapCommand { get; set; }
+        private readonly INavigationService _navigationService;
+        public readonly IPageDialogService _dialogService;
+        public MainPageViewModel(INavigationService navigationService,
+            IPageDialogService dialogService)
+        {
+            _navigationService = navigationService;
+            _dialogService = dialogService;
+
+            TapCommand = new DelegateCommand(async () =>
+            {
+                // 進行 Plugin.Media 套件的初始化動作
+                await Plugin.Media.CrossMedia.Current.Initialize();
+
+                // 確認這個裝置是否具有拍照的功能
+                if (!Plugin.Media.CrossMedia.Current.IsCameraAvailable || !Plugin.Media.CrossMedia.Current.IsTakePhotoSupported)
+                {
+                    await _dialogService.DisplayAlertAsync("No Camera", ":( No camera available.", "OK");
+                    return;
+                }
+
+                // 啟動拍照功能，並且儲存到指定的路徑與檔案中
+                var file = await Plugin.Media.CrossMedia.Current.TakePhotoAsync(new Plugin.Media.Abstractions.StoreCameraMediaOptions
+                {
+                    Directory = "Sample",
+                    Name = "Sample.jpg"
+                });
+
+                if (file == null)
+                    return;
+
+                // 讀取剛剛拍照的檔案內容，轉換成為 ImageSource，如此，就可以顯示到螢幕上了
+
+                MyImageSource = ImageSource.FromStream(() =>
+                {
+                    var stream = file.GetStream();
+                    file.Dispose();
+                    return stream;
+                });
+            });
+        }
+
+        public void OnNavigatedFrom(NavigationParameters parameters)
+        {
+
+        }
+
+        public void OnNavigatingTo(NavigationParameters parameters)
+        {
+
+        }
+
+        public void OnNavigatedTo(NavigationParameters parameters)
+        {
+
+        }
+
+    }
+}
