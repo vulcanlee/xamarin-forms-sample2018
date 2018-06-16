6bf878d5b030fbb97fb028df1f39bb8d3df29402
李進興 Vulcan <vulcan@miniasp.com>
2018-06-16 21:59:36 +0800

    Xamarin.Essentials 體驗 2 : Device Information 裝置資訊

---
+++ XFESDeviceInformation/XFESDeviceInformation/XFESDeviceInformation/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,57 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace XFESDeviceInformation.ViewModels
+{
+    using System.ComponentModel;
+    using Prism.Events;
+    using Prism.Navigation;
+    using Prism.Services;
+    using Xamarin.Essentials;
+
+    public class MainPageViewModel : INotifyPropertyChanged, INavigationAware
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public string Model { get; set; }
+        public string Manufacturer { get; set; }
+        public string Name { get; set; }
+        public string VersionString { get; set; }
+        public string Platform { get; set; }
+        public string Idiom { get; set; }
+        public string DeviceType { get; set; }
+        private readonly INavigationService _navigationService;
+
+        public MainPageViewModel(INavigationService navigationService)
+        {
+            _navigationService = navigationService;
+
+        }
+
+        public void OnNavigatedFrom(NavigationParameters parameters)
+        {
+
+        }
+
+        public void OnNavigatingTo(NavigationParameters parameters)
+        {
+
+        }
+
+        public void OnNavigatedTo(NavigationParameters parameters)
+        {
+            Model = DeviceInfo.Model;
+            Manufacturer = DeviceInfo.Manufacturer;
+            Name = DeviceInfo.Name;
+            VersionString = DeviceInfo.VersionString;
+            Platform = DeviceInfo.Platform;
+            Idiom = DeviceInfo.Idiom;
+            DeviceType = DeviceInfo.DeviceType.ToString();
+        }
+
+    }
+}
