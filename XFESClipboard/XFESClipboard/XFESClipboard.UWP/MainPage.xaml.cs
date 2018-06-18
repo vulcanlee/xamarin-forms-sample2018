940903ad975fdc6dc64002696ed0a06c0d89ae77
李進興 Vulcan <vulcan@miniasp.com>
2018-06-18 17:31:03 +0800

    Xamarin.Essentials 體驗 4 : Clipboard 剪貼簿

---
+++ XFESClipboard/XFESClipboard/XFESClipboard.UWP/MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using Prism;
+using Prism.Ioc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+namespace XFESClipboard.UWP
+{
+    public sealed partial class MainPage
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            Plugin.Toasts.UWP.ToastNotification.Init();
+
+            LoadApplication(new XFESClipboard.App(new UwpInitializer()));
+        }
+    }
+
+    public class UwpInitializer : IPlatformInitializer
+    {
+        public void RegisterTypes(IContainerRegistry container)
+        {
+            container.Register<Plugin.Toasts.IToastNotificator, Plugin.Toasts.UWP.ToastNotification>();
+        }
+    }
+}
---
+++ XFESClipboard/XFESClipboard/XFESClipboard/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,73 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace XFESClipboard.ViewModels
+{
+    using System.ComponentModel;
+    using Prism.Events;
+    using Prism.Navigation;
+    using Prism.Services;
+    using Xamarin.Essentials;
+
+    public class MainPageViewModel : INotifyPropertyChanged, INavigationAware
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public string MyEntry { get; set; }
+        public DelegateCommand<string> LabelTapCommand { get; set; }
+        public DelegateCommand RetriveClipboardCommand { get; set; }
+        private readonly INavigationService _navigationService;
+        public Plugin.Toasts.IToastNotificator _ToastNotificator { get; set; }
+        public MainPageViewModel(INavigationService navigationService,
+            Plugin.Toasts.IToastNotificator toastNotificator)
+        {
+            _navigationService = navigationService;
+            _ToastNotificator = toastNotificator;
+
+            LabelTapCommand = new DelegateCommand<string>(async (x) =>
+            {
+                Clipboard.SetText(x);
+                await _ToastNotificator.Notify(new Plugin.Toasts.NotificationOptions()
+                {
+                    Title = $"通知",
+                    Description = $"{MyEntry} 已經複製到剪貼簿中"
+                });
+            });
+
+            RetriveClipboardCommand = new DelegateCommand(async () =>
+            {
+                if( Clipboard.HasText )
+                {
+                    MyEntry = $"產生自剪貼簿:{await Clipboard.GetTextAsync()}";
+                }
+                else
+                {
+                    await _ToastNotificator.Notify(new Plugin.Toasts.NotificationOptions()
+                    {
+                        Title = $"警告",
+                        Description = $"剪貼簿內沒有任何資料"
+                    });
+                }
+            });
+        }
+
+        public void OnNavigatedFrom(NavigationParameters parameters)
+        {
+
+        }
+
+        public void OnNavigatingTo(NavigationParameters parameters)
+        {
+
+        }
+
+        public void OnNavigatedTo(NavigationParameters parameters)
+        {
+        }
+
+    }
+}
