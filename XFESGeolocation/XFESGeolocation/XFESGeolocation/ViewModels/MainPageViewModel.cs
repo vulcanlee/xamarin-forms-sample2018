0aa29d6bcffdfc2c37b94b22a32b593b4bb98f0f
李進興 Vulcan <vulcan@miniasp.com>
2018-06-20 18:37:20 +0800

    Xamarin.Essentials 體驗 5 : Geolocation 地理位置

--- XFESGeolocation/XFESGeolocation/XFESGeolocation/ViewModels/MainPageViewModel.cs
+++ XFESGeolocation/XFESGeolocation/XFESGeolocation/ViewModels/MainPageViewModel.cs
@@ -8,12 +8,19 @@ using System.Text;
 
 namespace XFESGeolocation.ViewModels
 {
+    using System.Collections.ObjectModel;
     using System.ComponentModel;
     using System.Threading;
     using Prism.Events;
     using Prism.Navigation;
     using Prism.Services;
     using Xamarin.Essentials;
+
+    public class GeolocationAccuracyItem
+    {
+        public string Name { get; set; }
+        public GeolocationAccuracy Item { get; set; }
+    }
 
     public class MainPageViewModel : INotifyPropertyChanged, INavigationAware
     {
@@ -25,19 +32,28 @@ namespace XFESGeolocation.ViewModels
         private readonly INavigationService _navigationService;
         public CancellationTokenSource CTS { get; set; }
         public CancellationToken Token { get; set; }
-
-        public MainPageViewModel(INavigationService navigationService)
+        public ObservableCollection<GeolocationAccuracyItem> GeolocationAccuracySource { get; set; } = new ObservableCollection<GeolocationAccuracyItem>();
+        public GeolocationAccuracyItem GeolocationAccuracySelected { get; set; }
+        public readonly IPageDialogService _dialogService;
+        public MainPageViewModel(INavigationService navigationService,
+            IPageDialogService dialogService)
         {
             _navigationService = navigationService;
+            _dialogService = dialogService;
             GetLocationCommand = new DelegateCommand(async () =>
             {
+                if(GeolocationAccuracySelected == null)
+                {
+                    await _dialogService.DisplayAlertAsync("錯誤", "請選擇GPS定位精確度", "確定");
+                    return;
+                }
                 CTS = new CancellationTokenSource();
                 Token = CTS.Token;
                 GetLocation = true;
 
                 try
                 {
-                    var request = new GeolocationRequest(GeolocationAccuracy.Medium, TimeSpan.FromSeconds(10));
+                    var request = new GeolocationRequest(GeolocationAccuracySelected.Item, TimeSpan.FromSeconds(10));
                     var location = await Geolocation.GetLocationAsync(request, Token);
 
                     if (location != null)
@@ -83,7 +99,16 @@ namespace XFESGeolocation.ViewModels
 
         public void OnNavigatedTo(NavigationParameters parameters)
         {
-
+            GeolocationAccuracySource.Clear();
+            var fooItems = Enum.GetValues(typeof(GeolocationAccuracy));
+            foreach (GeolocationAccuracy item in fooItems)
+            {
+                GeolocationAccuracySource.Add(new GeolocationAccuracyItem()
+                {
+                     Name = item.ToString(),
+                     Item = item,
+                });
+            }
         }
 
     }
