7855035562138321f5b247f3df6d55e65fd1d6ed
李進興 Vulcan <vulcan@miniasp.com>
2018-09-24 16:32:52 +0800

    Data Binding 之 ViewModel 的綁定與 BindableProperty 的綁定用法

---
+++ DataBindingDemo/DataBindingDemo/DataBindingDemo/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,24 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataBindingDemo.ViewModels
+{
+    public class MainPageViewModel : ViewModelBase
+    {
+        public MainPageViewModel(INavigationService navigationService)
+            : base(navigationService)
+        {
+            Name = "Hello";
+            MyName = "Wold";
+        }
+
+        public string Name { get; set; }
+        public string MyName { get; set; }
+
+    }
+}
---
+++ DataBindingDemo/DataBindingDemo/DataBindingDemo/Views/MyControl.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace DataBindingDemo.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MyControl : ContentView
+    {
+        public MyControl()
+        {
+            InitializeComponent();
+        }
+        public string MyName
+        {
+            get { return (string)GetValue(MyNameProperty); }
+            set { SetValue(MyNameProperty, value); }
+        }
+
+        public static readonly BindableProperty MyNameProperty =
+            BindableProperty.Create(nameof(MyName),
+                typeof(string),
+                typeof(MyControl),
+                null, BindingMode.TwoWay);
+
+    }
+}
